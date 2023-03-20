68dfa31613a90d318ff55c23041b00106bd27871
Felipe Fernandes <felipedfe@gmail.com>
2023-03-20 12:34:18 -0300

    excluding non used icons

--- src/components/ProjectLinks/ProjectLinks.jsx
+++ src/components/ProjectLinks/ProjectLinks.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { FaVimeo } from 'react-icons/fa';
-import { RiInstagramFill } from 'react-icons/ri';
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
 import { IoMdOpen } from 'react-icons/io';
 
 function ProjectLinks({urls}) {
   const { github, deploy } = urls;
-  console.log("_-_-_---->", github)
   return (
     <div className="icons-container">
       <a href={github} rel="noreferrer" target="_blank">
